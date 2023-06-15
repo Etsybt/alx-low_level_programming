d283193913132b2ff5eafd5690e398cbc7e5ddc8
Etsybt <hmidouchnouhaila2@gmail.com>
2023-06-15 15:01:42 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - entry
- * return: always 0 (success)
+ * main - entry "Programming is like building a multilingual puzzle
+ * Return: always 0 (success)
  */
 int main(void)
 {
