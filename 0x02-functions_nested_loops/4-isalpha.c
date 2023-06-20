88d17239992da531bc9c26981c675fef9ba1df03
Etsybt <hmidouchnouhaila2@gmail.com>
2023-06-20 15:18:08 +0000

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - checks if c is an alphabet
+ * @c: character in the ASCII code
+ * Return: 1 if it's a letter
+ * 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65  && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
