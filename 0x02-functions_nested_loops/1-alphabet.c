c0fa2efd8f67cde1a4a996de7a2f2535575fc782
Etsybt <hmidouchnouhaila2@gmail.com>
2023-06-20 14:34:39 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * main - print alphabet
+ * print_alphabet - print alphabet
+ * description: return lowercase alphabet
  * return: 0
  */
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 97; i <= 122; i++)
-	    {
-		    _putchar(i);
-	    }
+	{
+		_putchar(i);
+	}
 	_putchar('\n');
 }
