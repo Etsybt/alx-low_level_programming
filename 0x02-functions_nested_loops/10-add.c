723bb865c2868cd48b8d189df13c58315e586453
Etsybt <hmidouchnouhaila2@gmail.com>
2023-06-20 18:19:01 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - addition
+ * @a: integer
+ * @b: integer
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ */
+void times_table(void)
+{
+	for (a = 0; a 
+}
