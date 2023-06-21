e6b69f8b1a42d2518f2dd5b18cbd7c8c8fe8ca95
Etsybt <hmidouchnouhaila2@gmail.com>
2023-06-21 00:11:28 +0000

    m

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,3 +6,18 @@
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
+int main(void)
+{
+	        int a, b, c;
+		        int largest;
+
+			        a = 972;
+				        b = -98;
+					        c = 0;
+
+						        largest = largest_number(a, b, c);
+
+							        printf("%d is the largest number\n", largest);
+
+								        return (0);
+}
