44bb7cdbfb1a337c77d787755db1182223323bb2
Etsybt <hmidouchnouhaila2@gmail.com>
2023-06-22 16:23:21 +0000

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+  * main - main function
+  * description: printing from 1 to 100
+  * replacing mulitples if 3 by Fizz
+  * of 5 by Buzz
+  * of 3 and 5 by FizzBuzz
+  * Return: 0
+  */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", a);
+		}
+		if (a != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
