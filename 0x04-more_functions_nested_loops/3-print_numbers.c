28f57139a31390c6e8916c8c309330856777e4b7
Etsybt <hmidouchnouhaila2@gmail.com>
2023-06-22 13:53:47 +0000

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
   * print_numbers - prints from 0 to 9
-  * Return: 0
+  * Return: void
   */
 void print_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = 48; i <= 57; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
