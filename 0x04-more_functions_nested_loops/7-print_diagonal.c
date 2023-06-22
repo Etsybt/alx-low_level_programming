d80ae26d839cc64247aa9d2c963d6cc94efea4c3
Etsybt <hmidouchnouhaila2@gmail.com>
2023-06-22 15:26:24 +0000

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,7 +6,7 @@
   */
 void print_diagonal(int n)
 {
-	int a;
+	int a, b;
 
 	if (n <= 0)
 	{
@@ -16,8 +16,12 @@ void print_diagonal(int n)
 	{
 		for (a = 0; a < n; a++)
 		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(32);
+			}
 			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
