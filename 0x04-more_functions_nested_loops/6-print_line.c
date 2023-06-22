238994b1c5bc7e059540d9c1d6e2a1a92a5b13e2
Etsybt <hmidouchnouhaila2@gmail.com>
2023-06-22 15:13:36 +0000

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,15 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		_putchar('_');
-		if (n <= 0)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
-			_putchar('\n');
+			_putchar(95);
 		}
 		_putchar('\n');
 	}
