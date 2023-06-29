59eca1c0f46ccacd076f1f8f34d7cae9f0bbf2f7
Etsybt <hmidouchnouhaila2@gmail.com>
2023-06-29 15:59:34 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * *_strcat - concatenates two strings,
- * @dest: destination.
- * @src: source.
- * Return: dest;
+ * *_strcat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
