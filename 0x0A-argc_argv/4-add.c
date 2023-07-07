02acaee60639714732d9f4e91a36ef83336b6d3a
Etsybt <hmidouchnouhaila2@gmail.com>
2023-07-07 17:14:55 +0000

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+  * @argc: arg count
+  * @argv: arg vector
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
