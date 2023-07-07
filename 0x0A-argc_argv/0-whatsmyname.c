baacb1de5ec951a94221a80c4801e3d7038ea451
Etsybt <hmidouchnouhaila2@gmail.com>
2023-07-07 14:12:19 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * main - main function
+  * @argc: argument count
+  * @argv: argument vector
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+	return (0);
+}
