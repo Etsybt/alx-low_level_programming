88606dfe0a14afc6f5328650746fa3889c2167ee
Etsybt <hmidouchnouhaila2@gmail.com>
2023-07-07 13:24:16 +0000

    libmy.a

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+    * _strlen - returns the length of a string
+    * @s: string
+    * Return: a
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		a++;
+	}
+	return (a);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+   *  _abs - main
+    * @i: interger
+     * Return: i absolute value of i
+       */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+	_putchar('\n');
+}
