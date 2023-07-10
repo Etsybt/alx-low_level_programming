2719641e76dfd090ea5d02cd27fe5f98de786ecb
Etsybt <hmidouchnouhaila2@gmail.com>
2023-07-10 14:42:32 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,17 +15,13 @@ char *str_concat(char *s1, char *s2)
 
 	char *result = (char *) malloc((len1 + len2 + 1) * sizeof(char));
 
+	if (s1 == NULL)
+		return (s2);
+	if (s2 == NULL)
+		return (s1);
 	if (result == NULL)
 	{
 		return (NULL);
-	}
-	if (s1 == NULL)
-	{
-		return (s2);
-	}
-	if (s2 == NULL)
-	{
-		return (s1);
 	}
 	strcpy(result, s1);
 	strcat(result, s2);
