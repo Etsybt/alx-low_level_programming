9ecd671a8ece41cb32be4ff856ecdb620d8c6a23
Etsybt <hmidouchnouhaila2@gmail.com>
2023-07-10 15:12:32 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,35 +13,24 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	unsigned int i, j, len1, len2;
 
-	i = 0;
 	j = 0;
-	len1 = 0;
-	len2 = 0;
-
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+	s3 = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 	if (s3 == NULL)
 		return (NULL);
-	i = 0;
-	j = 0;
-
-	if (s1)
+	for (i = 0; s1[i]; != '\0'; i++)
+		s3[i] = s1[i];
+	for (; s2[j] != '\0'; i++)
 	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
+		s3[i] = s2[j];
+		j++;
 	}
-	if (s2)
-	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			i++;
-			j++;
-		}
-	}
-	s3[i] = '\0';
 	return (s3);
 }
