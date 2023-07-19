a930c25ae31b307ccf323a7bcc2a515daa046250
Etsybt <hmidouchnouhaila2@gmail.com>
2023-07-19 11:53:18 +0000

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
 /**
   * print_name - prints a name
   * @name: name
@@ -8,9 +8,6 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
-		return;
-	}
-	(*f)(name);
+	if (name != NULL || f != NULL)
+		f(name);
 }
