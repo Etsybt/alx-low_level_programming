463ef363e322600cd35b5f433ae311aeb49a7d1c
Etsybt <hmidouchnouhaila2@gmail.com>
2023-07-27 15:20:37 +0000

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
-#include "lists.h"
+#include <stdio.h>
 
+void __attribute__((constructor)) sentence(void);
 /**
-  * Hare - prints a str
+  * sentence - prints a str
   */
-void Hare(void)
+void sentence(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n")
