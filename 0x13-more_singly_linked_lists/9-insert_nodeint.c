772f846a64b13a1b4e392910b636fee2436fc379
Etsybt <hmidouchnouhaila2@gmail.com>
2023-07-31 23:39:30 +0000

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,6 +10,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *newNode = (listint_t *)malloc(sizeof(listint_t));
+	listint_t *insert = *head;
 	unsigned int i;
 
 	if (newNode == NULL)
@@ -17,33 +18,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newNode->n = n;
 
-	if (*head == NULL && idx == 0)
+	if (idx == 0)
 	{
-		newNode->next = NULL;
+		newNode->next = insert;
 		*head = newNode;
 		return (newNode);
 	}
 
-	if (*head != NULL && idx == 0)
+	for (i = 0; i < (idx - 1); i++)
 	{
-		newNode->next = *head;
-		*head = newNode;
-		return (newNode);
-	}
-
-	listint_t *current = *head;
-
-	for (i = 0; i < idx - 1; i++)
-	{
-		if (current == NULL)
-		{
-			free(newNode);
+		if (insert == NULL || insert->next == NULL)
 			return (NULL);
-		}
-		current = current->next;
+
+		insert = insert->next;
 	}
-	newNode->next = current->next;
-	current->next = newNode;
+	newNode->next = insert->next;
+	insert->next = newNode;
 
 	return (newNode);
 }
