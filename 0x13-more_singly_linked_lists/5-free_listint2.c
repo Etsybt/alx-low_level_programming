b47d9c2283883b60101897b186ba34b566d0e89f
Etsybt <hmidouchnouhaila2@gmail.com>
2023-07-31 19:10:40 +0000

    update

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - frees a listint_t list.
+  * @head: haad node
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;/* current node */
+	listint_t *temp;/* next node */
+
+	if (head == NULL)
+		return (NULL);
+
+	current = *head;
+	temp = (*head)->next;
+
+	while (temp)
+	{
+		free(current);
+		current = temp;
+		temp = temp->next;
+	}
+	free(current);
+	*head = NULL;
+}
