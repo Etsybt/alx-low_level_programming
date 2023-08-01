9440a5583b0419b31f0f59036a12e3f80075361f
Etsybt <hmidouchnouhaila2@gmail.com>
2023-08-01 11:22:17 +0000

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -14,7 +14,7 @@ size_t print_listint_safe(const listint_t *head)
 
 	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
+		printf("[%p] %i\n", (void *)current, current->n);
 		current = current->next;
 		count++;
 	}
