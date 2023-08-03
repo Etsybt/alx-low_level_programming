91bc7e65fc0f1182a0fbb042aaa64b0f29f1b07f
Etsybt <hmidouchnouhaila2@gmail.com>
2023-08-03 12:59:46 +0000

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,27 +7,20 @@
   */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num;
-	int l, base_2;
+	unsigned int num = 0;
+	int l;
 
-	if (b == NULL) 
+	if (!b) 
 		return (0);
-
-	num = 0;
 
 
 	for (l = 0; b[l] != '\0'; l++)
-		;
-	for (l--, base_2 = 1; l >= 0; l--, base_2 *= 2)
 	{
-		if (b[l] != '\0' && b[l] != '1')
+		if (b[l] < '\0' || b[l] > '1')
 		{
 			return (0);
 		}
-		if (b[l] & 1)
-		{
-			num += base_2;
-		}
+		num = 2 * num + (b[l] - '\0');
 	}
 	return (num);
 }
