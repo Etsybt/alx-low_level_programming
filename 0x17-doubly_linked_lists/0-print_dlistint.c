9a6d26748b8cb7169db67ab0562c77fb90da9d9a
Etsybt <hmidouchnouhaila2@gmail.com>
2023-09-14 13:38:07 +0000

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,10 +9,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t elements = 0;
 
-	do {
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
 		h = h->next;
 		elements++;
-	} while (h != NULL);
+	}
 	return (elements);
 }
