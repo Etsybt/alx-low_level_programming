ac1326e06e8978ed7da09142d5494f50979b24d5
Etsybt <hmidouchnouhaila2@gmail.com>
2024-01-16 14:51:05 +0000

    update

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -30,7 +30,6 @@ int interpolation_search(int *array, size_t size, int value)
 		else
 			high = (pos - 1);
 	}
-	
 	if (value > array[high])
 	{
 		printf("Value checked array[%lu] is out of range\n", high);
